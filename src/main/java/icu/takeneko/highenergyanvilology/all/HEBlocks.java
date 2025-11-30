a75d06069f7ae77e9bb9bbb4d19ee1ab3a0c1bd6
ZhuRuoLing <jkl-963@live.com>
2025-11-30 23:51:48 +0800

    datagen x3

--- src/main/java/icu/takeneko/highenergyanvilology/all/HEBlocks.java
+++ src/main/java/icu/takeneko/highenergyanvilology/all/HEBlocks.java
@@ -1,10 +1,17 @@
 package icu.takeneko.highenergyanvilology.all;
 
+import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import dev.dubhe.anvilcraft.init.block.ModBlockTags;
+import dev.dubhe.anvilcraft.init.item.ModItems;
 import icu.takeneko.highenergyanvilology.HEAnvilology;
 import icu.takeneko.highenergyanvilology.block.AnvilonEmitterBlock;
 import icu.takeneko.highenergyanvilology.util.ModelUtils;
 import icu.takeneko.highenergyanvilology.util.StateUtils;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.client.model.generators.ModelFile;
@@ -24,6 +31,14 @@ public class HEBlocks {
             .isSuffocating(StateUtils::never)
             .isViewBlocking(StateUtils::never)
         )
+        .tag(
+            BlockTags.INCORRECT_FOR_WOODEN_TOOL,
+            BlockTags.INCORRECT_FOR_GOLD_TOOL,
+            BlockTags.INCORRECT_FOR_STONE_TOOL,
+            BlockTags.NEEDS_IRON_TOOL,
+            BlockTags.MINEABLE_WITH_PICKAXE,
+            ModBlockTags.HAMMER_REMOVABLE
+        )
         .defaultBlockstate()
         .blockstate((ctx, cons) -> {
             cons.simpleBlock(
@@ -39,7 +54,7 @@ public class HEBlocks {
         .model((ctx, prov) -> {
             ModelUtils.wrapDefaultBlockItemTransform(
                 prov.getBuilder(ctx.getName())
-                .parent(new ModelFile.UncheckedModelFile("builtin/entity"))
+                    .parent(new ModelFile.UncheckedModelFile("builtin/entity"))
             );
         })
         .build()
@@ -49,6 +64,13 @@ public class HEBlocks {
         .block("titanium_alloy_block", Block::new)
         .initialProperties(() -> Blocks.NETHERITE_BLOCK)
         .tag(Tags.Blocks.STORAGE_BLOCKS, HETags.Blocks.STORAGE_BLOCKS_TITANIUM_ALLOY)
+        .tag(
+            BlockTags.INCORRECT_FOR_WOODEN_TOOL,
+            BlockTags.INCORRECT_FOR_GOLD_TOOL,
+            BlockTags.INCORRECT_FOR_STONE_TOOL,
+            BlockTags.NEEDS_IRON_TOOL,
+            BlockTags.MINEABLE_WITH_PICKAXE
+        )
         .item()
         .tag(Tags.Items.STORAGE_BLOCKS, HETags.Items.STORAGE_BLOCKS_TITANIUM_ALLOY)
         .build()
@@ -57,7 +79,25 @@ public class HEBlocks {
     public static final BlockEntry<Block> ROYAL_STEEL_CASING = HEAnvilology.REGISTRATE
         .block("royal_steel_casing", Block::new)
         .initialProperties(() -> Blocks.NETHERITE_BLOCK)
+        .properties(p -> p.strength(2f, 6.0F))
+        .tag(
+            BlockTags.INCORRECT_FOR_WOODEN_TOOL,
+            BlockTags.INCORRECT_FOR_GOLD_TOOL,
+            BlockTags.INCORRECT_FOR_STONE_TOOL,
+            BlockTags.NEEDS_IRON_TOOL,
+            BlockTags.MINEABLE_WITH_PICKAXE,
+            ModBlockTags.HAMMER_REMOVABLE
+        )
         .item()
+        .recipe((ctx, prov) -> {
+            ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ctx.get())
+                .pattern(" A ")
+                .pattern("A A")
+                .pattern(" A ")
+                .define('A', ModItems.ROYAL_STEEL_INGOT)
+                .unlockedBy("has_" + prov.safeName(ModItems.ROYAL_STEEL_INGOT), RegistrateRecipeProvider.has(ModItems.ROYAL_STEEL_INGOT))
+                .save(prov);
+        })
         .build()
         .register();
 
