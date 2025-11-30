f51ad4b8e23c5590b4f4cfeab35262031a27bfca
ZhuRuoLing <jkl-963@live.com>
2025-11-30 15:28:27 +0800

    eyelib rendering x4

--- src/main/java/icu/takeneko/highenergyanvilology/block/entity/AnvilonEmitterBlockEntity.java
+++ src/main/java/icu/takeneko/highenergyanvilology/block/entity/AnvilonEmitterBlockEntity.java
@@ -1,12 +1,19 @@
 package icu.takeneko.highenergyanvilology.block.entity;
 
+import dev.dubhe.anvilcraft.api.power.IPowerConsumer;
+import dev.dubhe.anvilcraft.api.power.PowerGrid;
 import dev.dubhe.anvilcraft.util.WatchableCyclingValue;
+import lombok.Getter;
+import lombok.Setter;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class AnvilonEmitterBlockEntity extends BlockEntity {
+public class AnvilonEmitterBlockEntity extends BlockEntity implements IPowerConsumer {
     private final WatchableCyclingValue<Float> rate = new WatchableCyclingValue<>(
         "working_rate",
         a -> {
@@ -14,11 +21,25 @@ public class AnvilonEmitterBlockEntity extends BlockEntity {
         0f, 0.8f, 1f, 2f, 3f, 5f
     );
 
+    @Getter
+    @Setter
+    private PowerGrid grid;
+
     public AnvilonEmitterBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
         super(type, pos, blockState);
     }
 
     public void onActivated() {
         rate.next();
+    }
+
+    @Override
+    public @Nullable Level getCurrentLevel() {
+        return level;
+    }
+
+    @Override
+    public @NotNull BlockPos getPos() {
+        return getBlockPos();
     }
 }
