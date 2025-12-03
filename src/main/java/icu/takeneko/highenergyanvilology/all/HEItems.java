f1a227de382af6d71f6ab55b5cd5d4bd9bd46513
猫猫鹰 <w84627302@gmail.com>
2025-12-03 19:44:56 +0800

    冷头 物品

--- src/main/java/icu/takeneko/highenergyanvilology/all/HEItems.java
+++ src/main/java/icu/takeneko/highenergyanvilology/all/HEItems.java
@@ -1,12 +1,18 @@
 package icu.takeneko.highenergyanvilology.all;
 
+import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import com.tterrag.registrate.util.entry.ItemEntry;
 import dev.dubhe.anvilcraft.init.item.ModItemTags;
+import dev.dubhe.anvilcraft.init.item.ModItems;
 import dev.dubhe.anvilcraft.recipe.anvil.wrap.SuperHeatingRecipe;
+import dev.dubhe.anvilcraft.util.DataGenUtil;
 import icu.takeneko.highenergyanvilology.HEAnvilology;
 import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.neoforge.common.Tags;
 
 public class HEItems {
     static {
@@ -32,6 +38,22 @@ public class HEItems {
         )
         .register();
 
+    public static final ItemEntry<Item> CRYOCOOLER = HEAnvilology.REGISTRATE
+            .item("cryocooler", Item::new)
+            .model(DataGenUtil::noExtraModelOrState)
+            .recipe((c, p) -> {
+                ShapedRecipeBuilder.shaped(RecipeCategory.MISC, c.get())
+                    .pattern("CDC")
+                    .pattern("ABA")
+                    .pattern(" C ")
+                    .define('A', Tags.Items.STORAGE_BLOCKS_COPPER)
+                    .define('B', Blocks.BLUE_ICE)
+                    .define('C', ModItemTags.GEMS_SAPPHIRE)
+                    .define('D', ModItems.CIRCUIT_BOARD)
+                    .unlockedBy("has_gem_sapphire", RegistrateRecipeProvider.has(ModItemTags.GEMS_SAPPHIRE))
+                    .save(p);
+            })
+            .register();
 
     public static void setupRegistration() {
     }
