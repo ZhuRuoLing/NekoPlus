426f231d19e158c5759ac25143abc096ba8c51ba
ZhuRuoLing <jkl-963@live.com>
2026-06-01 20:42:06 +0800

    new block x30.7

--- src/main/java/icu/takeneko/nekoplus/mixin/minecraft/LevelEventHandlerMixin.java
+++ src/main/java/icu/takeneko/nekoplus/mixin/minecraft/LevelEventHandlerMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.LevelEventHandler;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.level.block.LevelEvent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,16 +17,47 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LevelEventHandler.class)
 public class LevelEventHandlerMixin {
-    @Unique private static final int ANVIL_USE_EVENT = 1030;
-
-    @Shadow @Final private ClientLevel level;
+    @Shadow
+    @Final
+    private ClientLevel level;
 
     @Inject(method = "levelEvent", at = @At("HEAD"), cancellable = true)
     public void levelEvent(int eventType, BlockPos pos, int data, CallbackInfo ci) {
-        if (eventType == ANVIL_USE_EVENT) {
-            if (level.getBlockState(pos).is(NPBlocks.CAT_ANVIL)) { // if is cat anvil, play cat sound, not anvil sound.
-                level.playLocalSound(pos, NPSoundEvents.CAT_ANVIL_USE.get(), SoundSource.BLOCKS, 1f, 1f, false);
-                ci.cancel();
+        if (level.getBlockState(pos).is(NPBlocks.CAT_ANVIL)) {
+            switch (eventType) {
+                case LevelEvent.SOUND_ANVIL_BROKEN -> {
+                    level.playLocalSound(
+                        pos,
+                        NPSoundEvents.CAT_ANVIL_BREAK.get(),
+                        SoundSource.BLOCKS,
+                        1f,
+                        1f,
+                        false
+                    );
+                    ci.cancel();
+                }
+                case LevelEvent.SOUND_ANVIL_LAND -> {
+                    level.playLocalSound(
+                        pos,
+                        NPSoundEvents.CAT_ANVIL_LAND.get(),
+                        SoundSource.BLOCKS,
+                        1f,
+                        1f,
+                        false
+                    );
+                    ci.cancel();
+                }
+                case LevelEvent.SOUND_ANVIL_USED -> {
+                    level.playLocalSound(
+                        pos,
+                        NPSoundEvents.CAT_ANVIL_USE.get(),
+                        SoundSource.BLOCKS,
+                        1f,
+                        1f,
+                        false
+                    );
+                    ci.cancel();
+                }
             }
         }
     }
