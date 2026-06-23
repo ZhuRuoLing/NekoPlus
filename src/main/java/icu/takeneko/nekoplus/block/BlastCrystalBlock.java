7109543f4eb51e3b62558d943eb9d69a4f685662
ZhuRuoLing <jkl-963@live.com>
2026-06-24 06:40:03 +0800

    fix BlastCrystalBlock SHAPE

--- src/main/java/icu/takeneko/nekoplus/block/BlastCrystalBlock.java
+++ src/main/java/icu/takeneko/nekoplus/block/BlastCrystalBlock.java
@@ -9,20 +9,33 @@ import lombok.Getter;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jspecify.annotations.Nullable;
 
 import java.util.function.BiConsumer;
 
 public class BlastCrystalBlock extends BaseEntityBlock {
+
+    public static VoxelShape SHAPE = Shapes.or(
+        Block.box(0, 14, 0, 16, 16, 16),
+        Block.box(2, 2, 2, 14, 14, 14),
+        Block.box(0, 0, 0, 16, 2, 16)
+    );
+
+
     @Getter
     private final CrackStage stage;
 
@@ -73,6 +86,16 @@ public class BlastCrystalBlock extends BaseEntityBlock {
             return (BlockEntityTicker<T>) BlockEntityUtil.<BlastCrystalBlockEntity>createTicker();
         }
         return null;
+    }
+
+    @Override
+    public VoxelShape getShape(
+        BlockState state,
+        BlockGetter level,
+        BlockPos pos,
+        CollisionContext context
+    ) {
+        return SHAPE;
     }
 
     @Override
